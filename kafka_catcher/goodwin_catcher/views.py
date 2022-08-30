1169b2c031d87770ec424db20c2dd98b5488bf48
roman <tatarenko.roman@gmail.com>
2022-08-30 15:53:26 +0300

    Add new features: 1) Searching for messages in auto mode as JSON.

--- kafka_catcher/goodwin_catcher/views.py
+++ kafka_catcher/goodwin_catcher/views.py
@@ -2,6 +2,8 @@ import json
 import sqlite3
 
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 from some_functions import get_project_root
 
@@ -10,53 +12,55 @@ menu = ['Головна сторінка', 'Пошук повідомлень в
 
 
 def index(request):
-    return render(
-        request, 'goodwin_catcher/index.html',
-        {
-            'title': 'Головна сторінка'
-        }
-    )
+    if request.method == "GET":
+        return render(
+            request, 'goodwin_catcher/index.html',
+            {
+                'title': 'Головна сторінка'
+            }
+        )
 
 
 def manual_searching(request):
-    available_messages = "No messages yet..."
-    query_params = request.GET
-    if query_params:
+    if request.method == "GET":
+        available_messages = "No messages yet..."
+        query_params = request.GET
+        if query_params:
 
-        ocid = query_params['ocid']
-        operation_id = query_params['operation_id']
+            ocid = query_params['ocid']
+            operation_id = query_params['operation_id']
 
-        connection = sqlite3.connect(f"{root}/db.sqlite3")
-        cursor = connection.cursor()
+            connection = sqlite3.connect(f"{root}/db.sqlite3")
+            cursor = connection.cursor()
 
-        if ocid != "":
-            messages_list = list()
-            cursor.execute(f"""SELECT message FROM goodwin_catcher_message WHERE ocid = '{ocid}';""")
-            message = cursor.fetchall()
-            cursor.close()
+            if ocid != "":
+                messages_list = list()
+                cursor.execute(f"""SELECT message FROM goodwin_catcher_message WHERE ocid = '{ocid}';""")
+                message = cursor.fetchall()
+                cursor.close()
 
-            for msg in message:
-                messages_list.append(json.loads(msg[0]))
+                for msg in message:
+                    messages_list.append(json.loads(msg[0]))
 
-            available_messages = messages_list
+                available_messages = messages_list
 
-        elif operation_id != "":
-            cursor.execute(f"""SELECT message FROM goodwin_catcher_message WHERE x_operation_id = 
-            '{operation_id}';""")
+            elif operation_id != "":
+                cursor.execute(f"""SELECT message FROM goodwin_catcher_message WHERE x_operation_id = 
+                '{operation_id}';""")
 
-            message = cursor.fetchall()
-            cursor.close()
-            print("\nСПРОБА")
-            print(message)
-            available_messages = json.loads(message[0][0])
+                message = cursor.fetchall()
+                cursor.close()
+                print("\nСПРОБА")
+                print(message)
+                available_messages = json.loads(message[0][0])
 
-    return render(
-        request, 'goodwin_catcher/manual_searching.html',
-        {
-            'title': 'Пошук повідомлень в ручному режимі',
-            'messages': json.dumps(available_messages)
-        }
-    )
+        return render(
+            request, 'goodwin_catcher/manual_searching.html',
+            {
+                'title': 'Пошук повідомлень в ручному режимі',
+                'messages': json.dumps(available_messages)
+            }
+        )
 
 
 def support(request):
@@ -70,38 +74,27 @@ def support(request):
 
 
 def get_by_x_oper_id(request, x_operation_id):
-    connection = sqlite3.connect(f"{root}/db.sqlite3")
-    cursor = connection.cursor()
-    cursor.execute(f"""SELECT message FROM goodwin_catcher_message WHERE x_operation_id = '{x_operation_id}';""")
-    message = cursor.fetchall()
-    cursor.close()
+    if request.method == "GET":
+        connection = sqlite3.connect(f"{root}/db.sqlite3")
+        cursor = connection.cursor()
+        cursor.execute(f"""SELECT message FROM goodwin_catcher_message WHERE x_operation_id = '{x_operation_id}';""")
+        message = cursor.fetchall()
+        cursor.close()
 
-    return render(
-        request, 'goodwin_catcher/by_operation_id.html',
-        {
-            'title': 'Доступні повідомленння по x-operation-id:',
-            'messages': json.dumps(json.loads(message[0][0]))
-        }
-    )
+        response = json.loads(message[0][0])
+        return HttpResponse(json.dumps(response))
 
 
 def get_by_ocid(request, ocid):
-    messages_list = list()
-    connection = sqlite3.connect(f"{root}/db.sqlite3")
-    cursor = connection.cursor()
-    cursor.execute(f"""SELECT message FROM goodwin_catcher_message WHERE ocid = '{ocid}';""")
-    message = cursor.fetchall()
-    cursor.close()
+    if request.method == "GET":
+        messages_list = list()
+        connection = sqlite3.connect(f"{root}/db.sqlite3")
+        cursor = connection.cursor()
+        cursor.execute(f"""SELECT message FROM goodwin_catcher_message WHERE ocid = '{ocid}';""")
+        message = cursor.fetchall()
+        cursor.close()
 
-    for msg in message:
-        messages_list.append(json.loads(msg[0]))
+        for msg in message:
+            messages_list.append(json.loads(msg[0]))
 
-    messages_list = json.dumps(messages_list)
-
-    return render(
-        request, 'goodwin_catcher/by_ocid.html',
-        {
-            'title': 'Доступні повідомленння по ocid:',
-            'messages': messages_list
-        }
-    )
+        return HttpResponse(json.dumps(messages_list))
